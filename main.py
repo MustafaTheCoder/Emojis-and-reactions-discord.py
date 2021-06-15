3b321ae587d0974f2eba70df49395e0847a0cf14
MustafaTheCoder <73116655+MustafaTheCoder@users.noreply.github.com>
2021-06-15 22:49:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,3 @@
-import discord
-from discord.ext import commands
-
-client = commands.Bot(command_prefix="-")
-client.remove_command("help")
-
-async def ch_pr():
-    await client.wait_until_ready()
-    await client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Activity(
-        type=discord.ActivityType.watching,
-        name=f"{len(client.guilds)} Servers | -help"))
-
-
-client.loop.create_task(ch_pr())
-
-
-@client.event
-async def on_ready():
-    print("Bot is ready!")
-
-
-
-@client.command()
-async def Hello(ctx):
-    await ctx.send("Hi!")
-
-
-
-
-
-@client.group(invoke_without_command=True)
-@commands.cooldown(1, 10, commands.BucketType.user)
-async def help(ctx):
-    em = discord.Embed(
-        title="__**Help Command**__",
-        description="Your Description Here.", color=ctx.author.color)
-
-    em.add_field(name="Option(1)", value="Value(1)")
-    em.add_field(name="Option(2)", value="Value(2)")
-    em.add_field(name="Option(3)", value="Value(3)")
-    em.add_field(name="Option(4)", value="Value(4)")
-
-    await ctx.send(embed=em)
-
-@help.command()
-async def test(ctx):
-    await ctx.send("Success!")
-
-
-
-
-
-
-
 @client.command()
 async def test(ctx):
     await ctx.send("<:swag_kek:846442757542248487>") #Your emoji id here.
@@ -65,22 +11,4 @@ async def on_message(msg):
 
     await client.process_commands(msg) #This line is important dont forget to write it or else it wont work! 
     #What this line does is it process this event of checking the msg that we send so that it can react only on it and also process the help real command.
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.run("ODUyMjQzODQxODA4OTkwMjU4.YMD_7Q.7MlVsumyG1sI2hTlbBQ7YjtyOYY")
+   
